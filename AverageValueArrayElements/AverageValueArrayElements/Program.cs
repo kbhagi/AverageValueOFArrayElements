e7f15504e767c68ec3371a729f0d99db2362661f
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 17:10:14 +0530

    Add files via upload

---
+++ AverageValueArrayElements/AverageValueArrayElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AverageValueArrayElements
+{
+    class Program
+    {
+        public void sumAverageElements(int[] arr, int size)
+        {
+            int sum = 0; int average = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+            average = sum / size;
+            Console.WriteLine("Sum of Array is:" + sum);
+            Console.WriteLine("Average of Array is: " + average);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter the size:");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[size];
+            Console.WriteLine("Enter the elements of the Array:");
+            for (int i=0; i<size; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int len = a.Length;
+            Program pg = new Program();
+            pg.sumAverageElements(a, len);
+        }
+    }
+}
